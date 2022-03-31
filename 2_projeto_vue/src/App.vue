fd4076dea9345bbe3b85fba5e8556bcfced01311
Juscelino Cordeiro <jfilhodev@gmail.com>
2022-03-30 22:10:05 -0300

    manipulando componente

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -2,16 +2,19 @@
     <div>
         <h1>Olá Mundo!!!</h1>
         <PrimeiroComponente />
+        <LifeCycle />
     </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import LifeCycle from './components/LifeCycle.vue';
 
 export default {
     name: 'App',
     components: {
         PrimeiroComponente,
+        LifeCycle,
     },
 };
 </script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>meu nome é: {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda nao sei',
+        };
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = 'jusce';
+        }, 1000);
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Elesbao';
+        }, 3000);
+    },
+};
+</script>
