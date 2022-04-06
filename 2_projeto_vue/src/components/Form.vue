46af4feda974bbf618435de26dbaa48c633cd437
Juscelino Cordeiro <jfilhodev@gmail.com>
2022-04-05 21:46:32 -0300

    evento submit

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,15 +1,42 @@
 <template>
-    <form action="">
-        <div><Input /></div>
-        <div><Input /></div>
+    <div>
+        <h2>Meu Formulário</h2>
+    </div>
+    <form action="" @submit="enviarFormulario">
+        <div><input type="text" v-model="name" /></div>
+        <div><input type="text" v-model="email" /></div>
         <div><Submit /></div>
     </form>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
 </template>
 <script>
-import Input from './form/Input.vue';
 import Submit from './form/Submit.vue';
 export default {
     name: 'FormItem',
-    components: {Input, Submit},
+    components: {Submit},
+    data() {
+        return {
+            name: '',
+            email: '',
+        };
+    },
+    methods: {
+        enviarFormulario(e) {
+            const name = this.name;
+            const email = this.email;
+
+            e.preventDefault(); //inserir para evitar o relad da pagina
+            console.log('form enviado');
+            console.log('name = ' + name);
+            console.log('email = ' + email);
+        },
+    },
 };
 </script>
+<style scoped>
+p {
+    margin: 50px;
+}
+</style>
